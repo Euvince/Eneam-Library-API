8a01569b16344d1e9f9de631bf9da00d4b596b19
Euvince <danieleuvince2003@gmail.com>
2024-05-23 19:12:51 +0100

    Installation de Laravel Fortify et de Laravel Permissions

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\FortifyServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
