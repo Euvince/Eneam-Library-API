3574b7d96c3def28983f2ecdc8cc292340daaca3
Euvince <danieleuvince2003@gmail.com>
2024-06-18 16:09:30 +0100

    Retour sur Méthode *post* pour télécharger une fiche de dépôt 2

--- routes/Manager/api.php
+++ routes/Manager/api.php
@@ -102,7 +102,7 @@ Route::patch('reject-memory/{supportedMemory}', [SupportedMemoryController::clas
     ->name(name : 'reject-memory')
     ->where(['supportedMemory' => $idRegex]);
 
-Route::get(uri : 'print-filing-report/{supportedMemory}', action : [SupportedMemoryController::class, 'printFilingReport'])
+Route::post(uri : 'print-filing-report/{supportedMemory}', action : [SupportedMemoryController::class, 'printFilingReport'])
     ->name(name : 'print-filing-report');
 
 Route::post(uri : '/print-reports', action : [SupportedMemoryController::class, 'printReports'])
