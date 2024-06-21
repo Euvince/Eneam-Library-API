c35418a937dfb71d90f2b1928f3073bdfc0da3da
Euvince <danieleuvince2003@gmail.com>
2024-06-21 10:56:25 +0100

    Les demandes d'emprunts continuent

--- app/Http/Controllers/API/Loan/ManagerLoanController.php
+++ app/Http/Controllers/API/Loan/ManagerLoanController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\Loan\LoanResource;
 use App\Http\Responses\Loan\LoanCollectionResponse;
 use App\Http\Responses\Loan\SingleLoanResponse;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ManagerLoanController extends Controller
 {
@@ -29,21 +30,14 @@ class ManagerLoanController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(LoanRequest $request)
+    public function store(LoanRequest $request) : void
     {
-        $loan = Loan::create($request->validated());
-        return new SingleLoanResponse(
-            statusCode : 201,
-            allowedMethods : 'GET, POST, PUT, PATCH, DELETE',
-            message : "Votre demande d'emprunt a été envoyé avec succès",
-            resource : new LoanResource(resource : Loan::query()->with(['articles', 'user'])->where('id', $loan->id)->first())
-        );
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Loan $loan)
+    public function show(Loan $loan) : SingleLoanResponse
     {
         return new SingleLoanResponse(
             statusCode : 200,
@@ -56,7 +50,7 @@ class ManagerLoanController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(LoanRequest $request, Loan $loan)
+    public function update(LoanRequest $request, Loan $loan) : SingleLoanResponse
     {
         $loan->update($request->validated());
         return new SingleLoanResponse(
@@ -70,7 +64,7 @@ class ManagerLoanController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Loan $loan)
+    public function destroy(Loan $loan) : JsonResponse
     {
         $loan->delete();
         return response()->json(
--- app/Http/Controllers/API/Loan/UserLoanController.php
+++ app/Http/Controllers/API/Loan/UserLoanController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers\API\Loan;
 
-use App\Http\Controllers\Controller;
+use App\Models\Loan;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Loan\LoanResource;
+use App\Http\Responses\Loan\SingleLoanResponse;
 
 class UserLoanController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request) : SingleLoanResponse
     {
-
+        $loan = Loan::create($request->validated());
+        return new SingleLoanResponse(
+            statusCode : 201,
+            allowedMethods : 'GET, POST, PUT, PATCH, DELETE',
+            message : "Votre demande d'emprunt a été envoyé avec succès",
+            resource : new LoanResource(resource : Loan::query()->with(['articles', 'user'])->where('id', $loan->id)->first())
+        );
     }
 }
