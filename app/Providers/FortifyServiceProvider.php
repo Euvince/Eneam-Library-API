3eef481a5d4d5b4d7f337c54eb77a7569fb39f63
Euvince <danieleuvince2003@gmail.com>
2024-11-04 21:54:46 +0100

    Ré-exploration de quelques parties de  l'authentification

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -35,8 +35,11 @@ class FortifyServiceProvider extends ServiceProvider
         $this->app->instance(abstract : LoginResponse::class, instance : new AuthLoginResponse());
         $this->app->instance(abstract : LogoutResponse::class, instance : new AuthLogoutResponse());
         $this->app->instance(abstract : RegisterResponse::class, instance : new AuthRegisterResponse());
+        // POUR L'OPTION MOT DE PASSE OUBLIÉ ? LORS DE LA CONNEXION
         $this->app->instance(abstract : PasswordResetResponse::class, instance : new AuthPasswordResetResponse());
+        // POUR MODIFIER SON MOT DE PASSE UNE FOIS CONNECTÉ (AU NIVEAU DU PROFILE PAR EXEMPLE)
         $this->app->instance(abstract : PasswordUpdateResponse::class, instance : new AuthPasswordUpdateResponse());
+        // POUR MODIFIER LES INFORMATIONS DU PROFILE
         $this->app->instance(abstract : ProfileInformationUpdatedResponse::class, instance : new AuthProfileInformationUpdatedResponse());
     }
 
