d6449b035bc26e024226b0ec2e76c96396eebea6
Euvince CAPO CHICHI <danieleuvince20032gmail.com>
2025-08-12 00:23:41 +0100

    Little update

--- resources/views/mail/send-connection-credentials-to-user-mail.blade.php
+++ resources/views/mail/send-connection-credentials-to-user-mail.blade.php
@@ -10,3 +10,26 @@ Button Text
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
+
+
+# Bienvenue sur [Nom de l'application] !
+
+Bonjour [Nom de l'utilisateur],
+
+Votre compte a été créé avec succès sur [Nom de l'application]. Vous pouvez maintenant vous connecter en utilisant les identifiants ci-dessous :
+
+- **Nom d'utilisateur** : [username]
+- **Mot de passe** : [password]
+
+### Instructions de connexion
+
+1. Rendez-vous sur notre site : [Lien vers l'application].
+2. Entrez votre nom d'utilisateur et votre mot de passe.
+3. Pour des raisons de sécurité, nous vous recommandons de changer votre mot de passe après votre première connexion.
+
+Si vous rencontrez des difficultés pour accéder à votre compte, n'hésitez pas à contacter notre équipe d'assistance à l'adresse [email de support].
+
+Merci et bienvenue !
+
+Cordialement,
+L'équipe [Nom de l'application]
